42ae3f544b6ef4b26d2429807e646db178ef9cf6
Kellen Kolbeck & Trevor Scherling <student@epicodus.com>
2015-10-29 16:47:25 -0700

    sorting kinda works

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
 
   def index
-    @exercises = Exercise.order(params[:sort] + ' ' + params[:direction]).paginate(:page => params[:page], :per_page => 10)
-    @foods = Food.order(params[:sort] + ' ' + params[:direction]).paginate(:page => params[:page], :per_page => 10)
+    
+    @exercises = Exercise.order(params[:exercise_sort]).paginate(:page => params[:page], :per_page => 10)
+    @foods = Food.order(params[:food_sort]).paginate(:page => params[:page], :per_page => 10)
 
   end
+
 
 end
